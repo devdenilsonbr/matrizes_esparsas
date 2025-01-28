b1b0b5d23aae66ed4b4ea27861c6e66e7fb33162
Denilson <denilsonnunez098@gmail.com>
2025-01-28 17:34:11 -0300

    Test/testei na main as funcoes criadas para ver se tava funcioando

--- Matriz/main.cpp
+++ Matriz/main.cpp
@@ -4,7 +4,22 @@
 using namespace std;
 
 int main() { 
-    SparseMatrix matriz = SparseMatrix(3, 2);
+    SparseMatrix matriz = SparseMatrix(4, 3);
 
+    for (int i=1; i<=4; i++) {
+        for (int j = 1; j<=3; j++)
+        matriz.insert(i, j, i+j);
+    }
+
+    matriz.print();
+
+    SparseMatrix matriz1 = SparseMatrix(3, 3);
+    matriz1.insert(2, 1, 9);
+    matriz1.insert(1, 2, 7);
+
+    cout << "\n";
+
+    matriz1.print();
+    
     return 0;
 }
